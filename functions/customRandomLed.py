414f3741542a3d04513f83c5afe2490c0dcc1d27
Sverre <sverre@sverrecraft.com>
2019-04-01 10:04:21 +0200

    Rename and update with code from randomLed.py

--- functions/customRandomLed.py
+++ functions/customRandomLed.py
@@ -1,21 +1,21 @@
 import random
 import time
+import RPi.GPIO as GPIO
 from variables import led1, led2, led3, randomOnTime
-
 class randomLed:
     def __init__(self):
         #Generate random number between 1 and 3
         randomnumber = random.randint(1,3)
-        #Run this code for all pins
+        #Run code only for the pin selected by the random number
         if randomnumber == 1:
-            GPIO.output(37, GPIO.HIGH)
+            GPIO.output(led1, GPIO.HIGH)
             time.sleep(randomOnTime)
-            GPIO.output(37, GPIO.LOW)
+            GPIO.output(led1, GPIO.LOW)
         elif randomnumber == 2:
-            GPIO.output(38, GPIO.HIGH)
+            GPIO.output(led2, GPIO.HIGH)
             time.sleep(randomOnTime)
-            GPIO.output(38, GPIO.LOW)
+            GPIO.output(led2, GPIO.LOW)
         elif randomnumber == 3:
-            GPIO.output(40, GPIO.HIGH)
+            GPIO.output(led3, GPIO.HIGH)
             time.sleep(randomOnTime)
-            GPIO.output(40, GPIO.LOW)
+            GPIO.output(led3, GPIO.LOW)
